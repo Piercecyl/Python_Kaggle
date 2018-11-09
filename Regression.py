78f46dc08a0d17136950f3874498f6053a2ea0dc
Piercecyl <37281036+Piercecyl@users.noreply.github.com>
2018-11-09 15:23:59 +0800

    update to Tree.py

--- Regression.py
+++ Regression.py
@@ -2,44 +2,90 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
-import numpy as np
-all = pd.read_csv('D:/kaggle/House Price/all.csv')
-# Training data
-df_train = all.iloc[:1460, :]
-# Test data
-df_test = all.iloc[1460:, :]
-df_test = df_test.drop('SalePrice', axis=1)
+# --------------------------------------------------------Input data----------------------------------------------------
+train = pd.read_csv('D:/kaggle/House Price/processed_train.csv')
+test = pd.read_csv('D:/kaggle/House Price/processed_test.csv')
 # Split data
-X = df_train.drop('SalePrice', axis=1)
-y = df_train['SalePrice']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
-
-# ------------------------------------------------------------------------------------
-# Regression
+X = train.drop('SalePrice', axis=1)
+y = train['SalePrice']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=2)
+# ---------------------------------------------------------Regression---------------------------------------------------
 linreg = LinearRegression()
 y_pred = linreg.fit(X_train, y_train).predict(X_test)
-accuary_regression = linreg.score(X, y)
+accuary_regression = linreg.score(X_train, y_train)
 r_square_regression = metrics.explained_variance_score(y_test, y_pred)
-y_pred = linreg.predict(df_test)
-y_pred = pd.DataFrame(y_pred)
-y_pred.to_csv('D:/kaggle/House Price/regression.csv')
-# ------------------------------------------------------------------------------------
-# Lasso
-from sklearn.linear_model import Lasso
-lasso = Lasso(alpha=0.1)
+# -----------------------------------------------------------Lasso------------------------------------------------------
+from sklearn.linear_model import Lasso, LassoCV
+# Select the best alpha
+lasso_cv = LassoCV(alphas=[0.00009, 0.000091, 0.000099, 0.0001, 0.00002, 0.00015, 0.0002])
+lasso_cv.fit(X_train, y_train)
+# View alpha
+lasso_cv.alpha_
+#
+lasso = Lasso(alpha=0.00002, max_iter=50000)
 lasso.fit(X_train, y_train)
 y_pred_lasso = lasso.predict(X_test)
 accuary_lasso = lasso.score(X, y)
 r_square_lasso = metrics.explained_variance_score(y_test, y_pred_lasso)
-
-# ------------------------------------------------------------------------------------
-# ElasticNet
-from sklearn.linear_model import ElasticNet
-elasticn = ElasticNet(alpha=0.1, l1_ratio=0.7)
+# -------------------------------------------------------------Ridge----------------------------------------------------
+from sklearn.linear_model import Ridge
+ridge = Ridge(alpha=500)
+ridge.fit(X_train, y_train)
+y_pred_ridge = ridge.predict(X_test)
+accuary_ridge = lasso.score(X, y)
+r_square_ridge = metrics.explained_variance_score(y_test, y_pred_lasso)
+# -------------------------------------------------------------ElasticNet-----------------------------------------------
+from sklearn.linear_model import ElasticNet, ElasticNetCV
+# Select the best alpha
+ElasticNetCV_cv = ElasticNetCV(alphas=[0.00009, 0.000091, 0.000099, 0.0001, 0.00002, 0.00015, 0.0002],
+                               l1_ratio=[0.1, 0.2, 0.3, 0.4, 0.01, 0.02, 0.003, 0.6])
+ElasticNetCV_cv.fit(X_train, y_train)
+# View result
+ElasticNetCV_cv.alpha_
+ElasticNetCV_cv.l1_ratio_
+# Use best alpha and l1_ratio
+elasticn = ElasticNet(alpha=0.0002, l1_ratio=0.2, max_iter=50000)
 y_pred_ela = elasticn.fit(X_train, y_train).predict(X_test)
 accuary_elas = elasticn.score(X, y)
 r_square_ela = metrics.explained_variance_score(y_test, y_pred_ela)
+# ----------------------------------------------------------------Output------------------------------------------------
+from prettytable import PrettyTable
+result = PrettyTable(['model', 'R^2', 'Variance Score'])
+result.add_row(['Regression', accuary_regression, r_square_regression])
+result.add_row(['Lasso', accuary_lasso, r_square_lasso])
+result.add_row(['Ridge', accuary_ridge, r_square_ridge])
+result.add_row(['ElasticNet', accuary_elas, r_square_ela])
+print(result)
 
-print('Regression：', accuary_regression, r_square_regression)
-print('Lasso：', accuary_lasso, r_square_lasso)
-print('ElasticNet：', accuary_elas, r_square_ela)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tree.py
@@ -0,0 +1,81 @@
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.metrics import r2_score
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+# -----------------------------------------------------Input data-------------------------------------------------------
+train = pd.read_csv('D:/kaggle/House Price/processed_train.csv')
+test = pd.read_csv('D:/kaggle/House Price/processed_test.csv')
+# Split data
+X = train.drop('SalePrice', axis=1)
+y = train['SalePrice']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=2)
+# ----------------------------------------------------RandomForest------------------------------------------------------
+# Select parameter
+from sklearn.ensemble import RandomForestRegressor
+parameters = {
+    "n_estimators": [100,  200,  300, 400, 500] # Test out various amounts of trees in the forest
+}
+forest = RandomForestRegressor()
+grid = GridSearchCV(forest, parameters, n_jobs=-1)
+grid.fit(X_train, y_train)
+grid.score(X_test, y_test)
+grid.best_params_
+# Run Random forest
+ran_forest = RandomForestRegressor(n_estimators=400, n_jobs=2)
+y_pre_randomforest = ran_forest.fit(X_train, y_train).predict(X_test)
+random_score = ran_forest.score(X, y)
+random_r2 = r2_score(y_test, y_pre_randomforest)
+# ----------------------------------------------------XgBoost-----------------------------------------------------------
+# Parameter Adjustment
+from xgboost import XGBRegressor
+cv_params = {'learning_rate': [0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.7]}
+other_params = {'learning_rate': 0.1, 'n_estimators': 120, 'max_depth': 3, 'min_child_weight': 2, 'seed': 0,
+                    'subsample': 0.6, 'colsample_bytree': 0.7, 'gamma': 0.1, 'reg_alpha': 0.05, 'reg_lambda': 1}
+
+model = XGBRegressor(**other_params)
+optimized_GBM = GridSearchCV(estimator=model, param_grid=cv_params, scoring='r2', cv=5, verbose=1, n_jobs=4)
+optimized_GBM.fit(X_train, y_train)
+evalute_result = optimized_GBM.cv_results_
+print('樹數量：{0}'.format(optimized_GBM.best_params_))
+print('最佳模型得分:{0}'.format(optimized_GBM.best_score_))
+
+
+xg = XGBRegressor(learning_rate = 0.1, n_estimators = 120, max_depth = 3, min_child_weight = 2, seed = 0,
+                    subsample = 0.6, colsample_bytree = 0.7, gamma = 0.1, reg_alpha = 0.05, reg_lambda = 1)
+y_pre_xg = xg.fit(X_train, y_train).predict(X_test)
+xg_score = xg.score(X, y)
+xg_r2 = r2_score(y_test, y_pre_xg)
+# --------------------------------------------GradientBoostingRegressor-------------------------------------------------
+from sklearn.ensemble import GradientBoostingRegressor
+cv_params = {'n_estimators': [450, 500, 550]}
+other_params = {'n_estimators': 400, 'max_depth': 10, 'min_samples_split': 2}
+
+model = GradientBoostingRegressor(**other_params)
+optimized_GBM = GridSearchCV(estimator=model, param_grid=cv_params, scoring='r2', cv=5, verbose=1, n_jobs=-1)
+optimized_GBM.fit(X_train, y_train)
+evalute_result = optimized_GBM.cv_results_
+print('樹數量：{0}'.format(optimized_GBM.best_params_))
+print('最佳模型得分:{0}'.format(optimized_GBM.best_score_))
+
+gbr = GradientBoostingRegressor()
+y_pre_gbr = gbr.fit(X_train, y_train).predict(X_test)
+xg_score = xg.score(X, y)
+gbr_r2 = r2_score(y_test, y_pre_gbr)
+# -------------------------------------------------Output---------------------------------------------------------------
+from prettytable import PrettyTable
+result = PrettyTable(['model', 'accuracy', 'R^2'])
+result.add_row(['RandomForest', random_score, random_r2])
+result.add_row(['XgBoost', xg_score, xg_r2])
+result.add_row(['GradientBoostingRegressor', xg_score, gbr_r2])
+
+print(result)
+
+
+
+
+
+
+
+
+
